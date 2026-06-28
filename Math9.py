4fd643b50436b322fec16f0e84a2bbc534350378
Manisha <manishaghuge2008@gmail.com>
2026-06-28 23:51:22 +0530

    save current changes

--- Math9.py
+++ Math9.py
@@ -1,26 +1,24 @@
 import math
 
 try:
-    # Take input sentence
+
     sentence = input("Enter a sentence: ")
 
     if sentence.strip() == "":
         print("Empty sentence entered!")
     else:
-        # Convert sentence into words and store unique words in set
+
         words = sentence.lower().split()
         unique_words = set(words)
 
-        # Sort unique words
+    
         sorted_words = sorted(unique_words)
 
-        print("\nUnique words:", unique_words)
+        print("\n Unique words:", unique_words)
         print("Sorted unique words:", sorted_words)
-
-        # Count unique words
+  
         count = len(unique_words)
 
-        # Power using math module
         result = math.pow(count, 2)
 
         print("Total unique words:", count)
